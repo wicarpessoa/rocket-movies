d27672c65ac3a924cba18e564059bc8ac263b01f
unknown <wicar_pessoa@hotmail.com>
2023-01-27 13:50:16 -0300

    fixing imports for deploy

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 import { AuthRoutes } from "./auth.routes";
-import { AppRoutes } from "./App.routes";
+import { AppRoutes } from "./app.routes";
 import { useAuth } from "../hooks/auth";
 
 export function Routes() {
